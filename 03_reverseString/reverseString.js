744d0c18f40d3204c22e040059b83f8477118151
Victor Muhita <muhitavictor69@gmail.com>
2023-04-17 18:36:44 +0300

    third done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    const strLength = str.split(" ").length;
+
+    if( strLength === 1){
+        return str.split("").reverse().join("")
+    }else{
+        return str.split(" ").map((rev)=>rev.split("").reverse().join("")).reverse().join(" ")
+    }
 
 };
 
