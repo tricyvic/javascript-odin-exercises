14c60c05fc6d88f5b9a5689cf132d43acfd24145
Victor Muhita <muhitavictor69@gmail.com>
2023-04-17 18:19:35 +0300

    second done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(word , times) {
+    
+   if(times>=0){
+    return word.repeat(times)
+   }else
+    return 'ERROR'
 };
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
